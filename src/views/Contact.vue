2361d5ccc5a00e10b9c5d035cc7e152bc93e607d
ชื่nrsx006-afk <nrsx006@gmail.com>
2026-01-11 18:28:09 +0700

    update v2

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,9 +1,39 @@
 <template>
-    <div>
-    <h1>Contact to Narisa Muanjai</h1>
+    <div class="container">
+    <h1>Contact to Narisa Muanjai</h1> 
     <h3>Tel.091-775-3183</h3>  
     <img alt="Vue logo" src="../assets/Fruit-PNG-Photo.png">  
+
+
+
+<div class="container text-center">
+  <div class="row">
+    <div class="col-sm-6">
+      <img alt="Vue logo" src="../assets/SEA.jpg" height="400" width="550">  
     </div>
+
+    <div class="col-sm-6 bg-body-tertiary">
+<div class="mb-3 mt-4">
+  <label for="exampleFormControlInput1" class="form-label">Email address</label>
+  <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+</div>
+<div class="mb-3">
+  <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
+  <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+</div>
+<div class="mb-3 text-center">
+    <button type="submit" class="btn btn-success">Submit</button> &ensp;
+    <button type="reset" class="btn btn-danger">Cancel</button>
+
+</div>
+    </div>
+  </div>
+</div>
+
+<p></p>
+
+    </div>
+
 </template>
 
 
