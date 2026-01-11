4def241aee2f82d121bfe2a0729d70436e34ca5c
ชื่nrsx006-afk <nrsx006@gmail.com>
2026-01-11 16:43:18 +0700

    update v1

---
+++ src/views/Contact.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+    <h1>Contact to Narisa Muanjai</h1>
+    <h3>Tel.091-775-3183</h3>  
+    <img alt="Vue logo" src="../assets/Fruit-PNG-Photo.png">  
+    </div>
+</template>
+
+
+
+
+
